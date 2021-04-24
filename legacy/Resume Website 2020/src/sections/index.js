1dc9cdee975a7d04c17e55a24b13f23c21f82b33
David Benavidez <radicalralphhh@gmail.com>
2021-04-24 16:09:13 +0800

    chore: clean repo

--- legacy/Resume Website 2020/src/sections/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from './first-section';
-export { default as AboutMe } from './second-section';
-export { default as Fallback } from './fallback-section';
-export { default as Experience } from './third-section';
-export { default as FourthSection } from './fourth-section';
-export { default as Page404 } from './404';
